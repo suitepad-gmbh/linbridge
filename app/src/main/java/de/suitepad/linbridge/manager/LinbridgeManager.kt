6ef4c7aece1a8b070a1bbde60f6084fbb20e5e70
narges.dipir <nargesdipir@gmail.com>
2025-04-04 11:11:32 +0200

    enable srv option

--- app/src/main/java/de/suitepad/linbridge/manager/LinbridgeManager.kt
+++ app/src/main/java/de/suitepad/linbridge/manager/LinbridgeManager.kt
@@ -66,6 +66,7 @@ class LinbridgeManager @Inject constructor(
     override fun start() {
         core.isMicEnabled = true
         core.start()
+        core.isDnsSrvEnabled = true
         iterate()
     }
 
@@ -131,7 +132,6 @@ class LinbridgeManager @Inject constructor(
             return
         }
         proxyAddress.transport = TransportType.Udp
-        proxyAddress.port = -1
         proxyConfig.isRegisterEnabled = true
         proxyConfig.serverAddr = proxyAddress.asStringUriOnly()
         proxyConfig.identityAddress = address
