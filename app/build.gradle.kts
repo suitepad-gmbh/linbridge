cafa275cfb57344e6b9f0e43cc72bb1e6423aeef
narges dipir <nargesdipir@gmail.com>
2025-09-17 10:34:36 +0200

    2.0.6 (#30)
    * 2.0.6
    
    - FEAT: SRV Failover + remove deprecation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         minSdk = 23
         targetSdk = 33
 
-        versionCode = 2000540
-        versionName = "2.0.5"
+        versionCode = 2000640
+        versionName = "2.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SENDGRID_MAIL_TO", "\"${getDestinationEmailAddress()}\"")
