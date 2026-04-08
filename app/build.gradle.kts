00ee89375b30d825d69e05f8b9dc896df26e66b2
Frank Kohlhepp <git@frankkohlhepp.com>
2026-04-08 12:57:44 +0200

    2.1.1
    2.1.1
    
    - FIX: crash on Android 6
    - FIX: double ringtone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         minSdk = 23
         targetSdk = 33
 
-        versionCode = 2010040
-        versionName = "2.1.0"
+        versionCode = 2010140
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SENDGRID_MAIL_TO", "\"${getDestinationEmailAddress()}\"")
